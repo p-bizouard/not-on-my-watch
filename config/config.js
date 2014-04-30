c18b6b57646223f1cc314acbfcab9e44715f8a3d
Paul Bizouard <paul@ftweb.fr>
2014-04-30 18:52:25 +0200

    Add config.js for Heroku

---
+++ config/config.js
@@ -0,0 +1,62 @@
+
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+
+module.exports = {
+  development: {
+    db: process.env.MONGOHQ_URL,
+    root: rootPath,
+    app: {
+      name: 'Not on My Watch'
+    },
+    facebook: {
+      clientID: process.env.,  // Facebook application ID (PassportJs)
+      clientSecret: process.env.,
+      callbackURL: "http://" + process.env.HOST + "/auth/facebook/callback"
+    },
+    aws: {
+      accessKeyId: process.env.AWS_KEY, // Amazon web service key (ses)
+      secretAccessKey: process.env.AWS_SECRET,
+      region: process.env.AWS_REGION
+    },
+    mail_from: process.env.MAIL_FROM,
+    host: process.env.HOST,
+    crawl : {
+      url: process.env.CRAWL_URL,
+      selector: process.env.CRAWL_SELECTOR,
+      selector_title: process.env.CRAWL_SELECTOR_TITLE,
+      selector_subtitle: process.env.CRAWL_SELECTOR_SUBTITLE, // optionnal
+      selector_link: process.env.CRAWL_SELECTOR_LINK,
+      selector_body: process.env.CRAWL_SELECTOR_BODY,
+      nb_per_page:8 
+    }
+  },
+  production: {
+    db: process.env.MONGOHQ_URL,
+    root: rootPath,
+    app: {
+      name: 'Not on My Watch'
+    },
+    facebook: {
+      clientID: process.env.,  // Facebook application ID (PassportJs)
+      clientSecret: process.env.,
+      callbackURL: "http://" + process.env.HOST + "/auth/facebook/callback"
+    },
+    aws: {
+      accessKeyId: process.env.AWS_KEY, // Amazon web service key (ses)
+      secretAccessKey: process.env.AWS_SECRET,
+      region: process.env.AWS_REGION
+    },
+    mail_from: process.env.MAIL_FROM,
+    host: process.env.HOST,
+    crawl : {
+      url: process.env.CRAWL_URL,
+      selector: process.env.CRAWL_SELECTOR,
+      selector_title: process.env.CRAWL_SELECTOR_TITLE,
+      selector_subtitle: process.env.CRAWL_SELECTOR_SUBTITLE, // optionnal
+      selector_link: process.env.CRAWL_SELECTOR_LINK,
+      selector_body: process.env.CRAWL_SELECTOR_BODY,
+      nb_per_page:8 
+    }
+  },
+}
